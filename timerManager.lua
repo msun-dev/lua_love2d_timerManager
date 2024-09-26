dbf0cd0869d6727630b49b32e9ddc18935f9b4da
msun-dev <gmchofen@gmail.com>
2024-09-26 22:20:47 +0500

    timerManager created. Kinda works

---
+++ timerManager.lua
@@ -0,0 +1,41 @@
+require("timer")
+
+TimerManager = {
+    uuid = "",
+    timers = {}
+}
+TimerManager.__index =  TimerManager
+TimerManager.__type = "TimerManager"
+
+function TimerManager:initiate()
+    local object = {}
+    setmetatable(object, TimerManager)
+    return object
+end
+
+function TimerManager:create_timer(settings)
+    local timer = Timer:new(settings)
+    self.timers[timer.uuid] = timer
+    return timer.uuid
+end
+
+function TimerManager:remove_timer(name)
+    -- self.timers.remove(timer) ?
+end
+
+function TimerManager:get_timer(timer_name)
+    return self.timers[timer_name]
+end
+
+function TimerManager:update(delta)
+    -- for every timer in self.timers
+        -- timer:update(delta) 
+        -- if timer.ended and timer.repeating() then self:remove_timer(timer) end
+        -- else timers.remove(timer)
+end
+
+function TimerManager:__tostring()
+    for timer in self.timers do
+        print("\tCurrent timers:\n"..string.format(timer:__tostring().."\n"))
+    end
+end
