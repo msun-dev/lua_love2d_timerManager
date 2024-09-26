52299885fdfd05d16d4fde412a9a55fdbeca0960
msun-dev <gmchofen@gmail.com>
2024-09-26 23:54:20 +0500

    main.lua: 	using new modules in main code

--- main.lua
+++ main.lua
@@ -1,16 +1,31 @@
 -- Imports
 require("color")
+require("timer")
+require("timerManager")
 
 -- Main functions
 function love.load()
-    -- Global colors
+    -- Global variables
     Color_background = Color:random()
+    Timer_manager = TimerManager:initiate()
+
+    -- Timers
+    Timer_1sec = Timer_manager:create_timer({
+        duration = 1,
+        autostart = true,
+        repeating = true,
+        callback = function ()
+            Color_background:randomize_rgb()
+        end
+    })
 end
 
 function love.draw()
-    love.graphics.clear(Color_background:rgba())
+    love.graphics.clear(Color_background:rgb())
 end
 
-function love.update()
-    Color_background:randomize_rgb()
+function love.update(delta)
+    Timer_manager:update(delta)
+    --print(Timer_manager)
 end
+
