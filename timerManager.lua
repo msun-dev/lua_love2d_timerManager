dc46f48cf661c3e1b7cb4421412e937eecc087e2
msun-dev <gmchofen@gmail.com>
2024-10-03 16:53:41 +0500

    Revert "Added name field"
    This reverts commit b7d38a41441909df2248d205b736ffd4217dd248.

--- timerManager.lua
+++ timerManager.lua
@@ -1,18 +1,16 @@
 require("timer")
 
-local template = ("Timer-xxxxxxxx")
+local template = ("Timer-xxxxxxxx-xxxx-xxxx-xxxxxxxx")
 
 TimerManager = {
-    name = "TimerManager",
     timers = {}
 }
 TimerManager.__index =  TimerManager
 TimerManager.__type = "TimerManager"
 
-function TimerManager:new(settings)
+function TimerManager:new()
     local object = {}
     setmetatable(object, TimerManager)
-    if settings.name then self.name = settings.name end
     return object
 end
 
@@ -34,10 +32,7 @@ end
 function TimerManager:update(delta)
     for i, timer in pairs(self.timers) do
         timer:update(delta)
-        if timer:get_timeleft() < 0
-            and not timer.repeating
-            and timer.autoremove
-            then self:remove_timer(timer.uuid) end
+        if timer:get_timeleft() < 0 and not timer.repeating then self:remove_timer(timer.uuid) end
     end
 end
 
@@ -50,7 +45,7 @@ function TimerManager:gen_uuid()
 end
 
 function TimerManager:__tostring()
-    local out = string.format(self.name.."Current timers:\n")
+    local out = "Current timers:\n"
     for i, timer in pairs(self.timers) do
         out = string.format(out.."\t"..timer:__tostring().."\n")
     end
