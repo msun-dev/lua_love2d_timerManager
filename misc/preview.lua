48771b0979d4847bbf1fe82d070b7c98f5ca4503
msun-dev <gmchofen@gmail.com>
2024-10-18 17:40:57 +0500

    preview: init commit

---
+++ misc/preview.lua
@@ -0,0 +1,28 @@
+-- Only for the sake of preview.
+-- You can safely remove this file.
+
+local foreground_padding = 2 -- Add support
+local background_color = {255, 255, 255} -- Change to outline
+
+function DrawProgressBar(fraction, position, size, color)
+    -- percent filled: number - 95; 12.5; etc.
+    -- position: {x, y}
+    -- size: {w, h}
+    -- color: {r, g, b}
+    draw_background(position, size)
+    draw_progress(fraction, position, size, color)
+end
+
+function draw_background(position, size)
+    -- Drawmode.line
+    love.graphics.setColor(background_color)
+    love.graphics.rectangle("line", position.x, position.y, size.w, size.h)
+end
+
+function draw_progress(fraction, position, size, color)
+    love.graphics.setColor(color)
+    love.graphics.rectangle("fill",
+                            position.x + foreground_padding, position.y + foreground_padding,
+                            size.w * fraction - foreground_padding * 2, size.h - foreground_padding * 2)
+                            -- add clamp
+end
