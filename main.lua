48d9e273abbda1179736c4310e2b854de07bd656
msun-dev <gmchofen@gmail.com>
2024-10-18 17:36:58 +0500

    main: new preview

--- main.lua
+++ main.lua
@@ -1,6 +1,7 @@
 -- Imports
-require("timer")
-require("timerManager") -- Optionally you can only import timerManager
+require("timerManager.timerManager")
+require("timerManager.timer") -- Optionally you can only import timerManager
+require("misc.preview") -- Preview logic
 
 -- Initialising timer managers
 Timer_manager_default = TimerManager.new({name="TimerManager-Default"})
@@ -8,9 +9,9 @@ Timer_manager_chain = TimerManager.new({name="TimerManager-Chain"})
 
 function love.load()
     -- Love2D settings
-    love.window.setMode(900, 250)
+    love.window.setMode(900, 350)
     love.window.setTitle("Title Manager preview")
-    Font = love.graphics.newFont("Verdana.ttf", 12)
+    Font = love.graphics.newFont("misc/Verdana.ttf", 12)
 
     -- Timers insitialisation
     -- Simple timers
@@ -67,8 +68,38 @@ end
 
 function love.draw()
     -- Debug info
+    love.graphics.setColor({255,255,255})
     love.graphics.setFont(Font)
-    love.graphics.print(Timer_manager_default:__tostring(), 0, 0)
-    love.graphics.print(Timer_manager_chain:__tostring(), 0, 75)
-end
 
+    -- Default timer manager
+    love.graphics.print(Timer_manager_default:__tostring(), 0, 0)
+    local timers = Timer_manager_default:get_timers()
+    local i = 0
+    for key, timer in pairs(timers) do
+        love.graphics.print(timer:get_uuid(), 25 + (200 * i), 75)
+        love.graphics.print(string.format("Duration: ".. timer:get_duration()), 25 + (200 * i), 87)
+        love.graphics.print(string.format("Time left: " .. timer:get_timeleft()), 25 + (200 * i), 99)
+        local progress = timer:get_timeleft() / timer:get_duration()
+        DrawProgressBar(progress,
+                        {x=25 + (200 * i), y=115}, -- Yeah-yeah, magic numbers, I know
+                        {w=100, h=25},
+                        {100,100,100, 128})
+        i = i + 1
+    end
+
+    -- Chain timer manager
+    love.graphics.print(Timer_manager_chain:__tostring(), 0, 150)
+    local timers = Timer_manager_chain:get_timers()
+    local i = 0
+    for key, timer in pairs(timers) do
+        love.graphics.print(timer:get_uuid(), 25 + (200 * i), 75 + 150)
+        love.graphics.print(string.format("Duration: ".. timer:get_duration()), 25 + (200 * i), 87 + 150)
+        love.graphics.print(string.format("Time left: " .. timer:get_timeleft()), 25 + (200 * i), 99 + 150)
+        local progress = timer:get_timeleft() / timer:get_duration()
+        DrawProgressBar(progress,
+                        {x=25 + (200 * i), y=115 + 150}, -- Yeah-yeah, magic numbers, I know
+                        {w=100, h=25},
+                        {100,100,100, 128})
+        i = i + 1
+    end
+end
